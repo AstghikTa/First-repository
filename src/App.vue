ee0c90a9f7a19e6ae8060c848c2a2bc76550939c
Astghik Tadevosyan <astx496@gmail.com>
2024-04-07 01:09:31 +0400

    Creted Exchange currency conversion platform

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,17 @@
 <script >
+import Exchange from './components/Exchange.vue';
 
 export default{
-data(){
-  return{
-email: '',
-isEmailValid: false,
-  };
-},
-
-methods: {
-  openEmailPrompt(){
-    let email = prompt('input your email');
-    if(!email){
-this.isEmailValid = false;      
-return;
-    } 
-    email = email.trim();
-    this.email = email;
-   const isValid = this.checkIsEmailValid(email);
-   this.isEmailValid = isValid;
+  components: {
+    Exchange
   },
- checkIsEmailValid(email){
-    const regEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
-    return regEmail.test(email);
-  },
-  reset(){
-    this.email = '';
-    this.isEmailValid = false;
-  }
-}
-}
+};
 
 </script>
 
 <template>
   <main>
    
-<button class="emailInputBtn" @click="openEmailPrompt()">Input your email</button>
-<button class="restBtn" @click="reset">Reset</button>
-<div v-if="email">
-  <h1 class="green" v-if="isEmailValid">{{ email }}</h1>
-<h1 class="red" v-else>{{ email }}</h1>
-</div>
-
-<h4 class="validText" v-if="email">{{ `The email is ${isEmailValid ? 'valid': 'invalid'}` }}</h4>
-
+<Exchange />
   </main>
 </template>
-
-<style scoped>
-.red{
-  border: 1px solid red;
-  margin-top: 20px;
-}
-
-.green{
-  border: 1px solid green;
-  margin-top: 20px;
-}
-.validText{
-  margin-top: 20px;
-}
-.emailInputBtn{
-  margin: 15px;
-  height: 40px;
-  width: 300px;
-}
-.restBtn{
-  margin: 15px;
-  height: 40px;
-  width: 100px;
-}
-</style>
---
+++ src/components/Exchange.vue
@@ -0,0 +1,78 @@
+<script>
+export default{
+    data(){
+        return{
+  value: 0,   
+  rate: 400,
+  mode: 'AMD'       
+        };
+    },
+methods: {
+    handleInput(event){
+        this.value = event.target.value;
+    },
+handleInputFocus(){
+    if(+this.value === 0){
+        this.value = '';
+    }
+},
+toggleMode(){
+    if(this.mode === 'AMD'){
+        this.mode = 'USD';
+    }else{
+        this.mode = 'AMD';
+    }
+    this.value = 0;
+}
+}
+
+}
+
+</script>
+
+
+<template>
+    <div v-if="mode==='AMD'"> 
+<h4 class="text"> AMD -> USD </h4>
+<input class="inputBtn" type="number" 
+:value="value"  
+@click="handleInput" 
+@focus="handleInputFocus">
+<input class="inputBtn" type="number" readonly :value="value/rate">
+
+<button class="toggle" @click="toggleMode">USD -> AMD </button>
+</div>
+
+<div v-else>
+<h4 class="text"> USD -> AMD </h4>
+<input class="inputBtn" type="number" 
+:value="value"  
+@click="handleInput" 
+@focus="handleInputFocus">
+<input class="inputBtn" type="number" readonly :value="value*rate">
+
+<button class="toggle" @click="toggleMode">AMD -> USD </button>
+</div>
+
+</template>
+
+
+<style>
+.text{
+    margin-left: 117px;
+   color: crimson;
+font-size: x-large;
+}
+.inputBtn{
+    align-items: center;
+    margin: 15px;
+    height: 30px;
+}
+.toggle{
+background-color: rgb(110, 185, 110);
+/* margin-left: 300px; */
+/* margin-top: auto;
+margin-right: 500px; */
+
+}
+</style>
